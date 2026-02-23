c0270353ab32d24ee290bce9eea3a0f74e23dbf8
Osama Azmat Khan <usama.azmat@hotmail.com>
2026-02-23 15:20:50 +0900

    Initial Commit

---
+++ PFListy/PFListyApp.swift
@@ -0,0 +1,21 @@
+//
+//  PFListyApp.swift
+//  PFListy
+//
+//  Created by Osama Azmat Khan on 2026/02/23.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct PFListyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
